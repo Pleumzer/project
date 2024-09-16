0a967620c924e6767a0160dfa08a2d792c9edd4c
Chinnapatz <mr.chronos038@gmail.com>
2024-09-16 18:11:59 +0700

    "Close #18  - register backend"

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';  // Import FormsModule
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -50,6 +50,7 @@ import { QuestionsDetailComponent } from './pages/questions/questions-detail/que
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    HttpClientModule,
     ToastrModule.forRoot({
       positionClass: 'toast-top-center', 
       timeOut: 2000, 
